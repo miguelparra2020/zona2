a1863617a9c497ddca5e3d93e33203c76c92c632
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-07-31 18:59:52 -0500

    Otro Cambio footer

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Hola soy Bocanegra
+segundo cambio
--- components/FooterLandPage.js
+++ components/FooterLandPage.js
@@ -1,11 +1,13 @@
 // components/Navbar.js
 import React from 'react';
+import '../public/styles/FooterLandPage.css'
 
 const FooterLandPage = () => {
   return (
-    
-      <div>
-            Footer del LandPage
+      <div className='container-footer'>
+        <h1 className='footer__h1'>Hola Esto es un cambio</h1>
+            Todos los derechos reservados
+            Copyright c 2023 - SENA CDIT / ADSO - 2465417
       </div>
   );
 };
