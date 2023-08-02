f172d71f8c0b560f03f473a6a109302410e166d8
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-08-01 19:24:30 -0500

    Prueba cambios navbarLandPage

--- components/NavbarLandPage.js
+++ components/NavbarLandPage.js
@@ -1,20 +1,33 @@
 // components/Navbar.js
 import React from 'react';
 import Link from 'next/link';
+import "/public/images/logo_negro2.png";
 
 const NavbarLandPage = () => {
   return (
     <nav>
       <div>
         <Link href="/">
-          
+            <h1>Cambio</h1>
+            <div>Img</div>
+            <span>Biometric Services</span>
+
         </Link>
+        <div className="header">
+        <nav className="nav">
+            <img src="public/images/logo_negro2.png" alt="" className="nav__img"></img>
+            <div className="nav__links">
+                <a href="" className="nav__link">
+                    Iniciar sesión
+                </a>
+                <a href="/" className="nav__link nav__link--activate">
+                    Registrarse
+                </a>
+            </div>
+        </nav>
+        </div>
       </div>
-      <div>
-        <Link href="/login">
-          Iniciar Sesión
-        </Link>
-      </div>
+
     </nav>
   );
 };
