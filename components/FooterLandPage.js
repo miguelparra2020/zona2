bbb5da0077c2d551d32dc4769a000ec533228a63
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-08-02 12:44:24 -0500

    Footer probicional

--- components/FooterLandPage.js
+++ components/FooterLandPage.js
@@ -4,9 +4,13 @@ import '../public/styles/FooterLandPage.css';
 
 const FooterLandPage = () => {
   return (
-      <div className='container-footer'>
-            Todos los derechos reservados
-            Copyright c 2023 - SENA CDIT / ADSO - 2465417
+      <div>
+        <footer className='footer'>
+          <p className='footer__p'>
+              Todos los derechos reservados
+              Copyright c 2023 - SENA CDIT / ADSO - 2465417
+          </p>
+        </footer>
       </div>
   );
 };
