f7785f9467c49d156c04bde9c35a4b3e68cfab95
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-08-04 20:47:07 -0500

    Añadi el main

---
+++ pages/index.jsx
@@ -0,0 +1,29 @@
+// pages/index.js
+import React from 'react';
+import  LandPageLayout from '../components/layouts/LandPageLayout';
+import styles from '@/styles/index.module.css';
+// Nota: Siempre importar etiquetas raras
+import Image from 'next/image';
+
+const LandPage = () => {
+  return (
+    <LandPageLayout>
+      <main className={styles.main}>
+      {/* className={styles.} */}
+        <section className={styles.section}>
+          <div className={styles.section__texts}>
+            <h1 className={styles.section__title}>Biometric Service</h1>
+            <p className={styles.section__p}> 
+                    "Lleve un control exhaustivo de su asistencia mientras inculca el hábito de llegar a tiempo." 
+            </p>
+          </div>
+          <div className={styles.section__mockup}>
+            <Image className={styles.section__img} src="/img/maqueta.png" alt="Logo Símbolo SENA y Biometric Service Negro" width={1000} height={670}/>
+          </div>
+        </section>
+      </main>
+    </LandPageLayout>
+  );
+};
+
+export default LandPage;
