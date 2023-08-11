b4d7fe3302cb90f354f8d20004ff42ebe7db8f35
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-08-11 07:30:09 -0500

    ultimo commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,10 +12,16 @@ const LandPage = () => {
       {/* className={styles.} */}
         <section className={styles.section}>
           <div className={styles.section__texts}>
+            {/* Título  */}
+            <div className={styles.container__title}>
             <h1 className={styles.section__title}>Biometric Service</h1>
-            <p className={styles.section__p}> 
-                    Lleve un control exhaustivo de su asistencia mientras inculca el hábito de llegar a tiempo.
-            </p>
+            </div>
+            {/* Parrafo  */}
+            <div className={styles.container__p} >
+              <p className={styles.section__p}> 
+                      Lleve un control exhaustivo de su asistencia mientras inculca el hábito de llegar a tiempo.
+              </p>
+            </div>
           </div>
           <div className={styles.section__mockup}>
             <Image className={styles.section__img} src="/img/maqueta.png" alt="Logo Símbolo SENA y Biometric Service Negro" width={1000} height={670}/>
@@ -27,6 +33,7 @@ const LandPage = () => {
           {/* ============= Cartas ========== */}
           <div className={styles.first__container}>
             <div className={styles.card__left__blue}>
+
               <h2 className={styles.card__title}>Aprendiz</h2>
               <h3 className={styles.card__subtitle}>Comunicación efectiva</h3>
               <p className={styles.card__description}>Comunica tus inconvenientes fácilmente. Anexa excusas, el instructor  las valida, garantizando que puedas continuar con confianza.</p>
