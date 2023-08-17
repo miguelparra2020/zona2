778103e101f5d4543d9a2ab5915d6204e136dd5a
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-08-16 22:24:25 -0500

    Ultimo

--- components/NavbarMain.js
+++ components/NavbarMain.js
@@ -1,38 +1,42 @@
 // components/NavbarLogin.js
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import styles from '@/styles/components/NavbarMain.module.css';
+import '../styles/global.css';
+
 
 const NavbarMain = () => {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <div>
-        <Link href="/home">     
+        <Link href="/home" className={styles.nav__link}>     
             <div>Img</div>
             <span>Biometric Services</span>
         </Link>
       </div>
-      <div>
-        <Link href="/excusas">     
+      <div >
+        <Link href="/excusas" className={styles.nav__link}>     
             <div>Excusas</div>
         </Link>
       </div>
       <div>
-        <Link href="/fichas">     
+        <Link href="/fichas" className={styles.nav__link}>     
             <div>Fichas</div>
         </Link>
       </div>
       <div>
-        <Link href="/usuarios">     
+        <Link href="/usuarios" className={styles.nav__link}>     
             <div>Usuarios</div>
         </Link>
       </div>
       <div>
-        <Link href="https://miguelpaez9612.pythonanywhere.com/admin/" target='_blank'>     
+        <Link href="https://miguelpaez9612.pythonanywhere.com/admin/" target='_blank' className={styles.nav__link}>     
             <div>Administración</div>
         </Link>
       </div>
       <div>
-        <Link href="/">     
+        <Link href="/" className={styles.nav__link}>     
             <div>Salir</div>
         </Link>
       </div>
