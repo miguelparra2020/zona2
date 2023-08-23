caabd3c6dcb0e2fc5647671b38440bd92264097d
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-08-23 08:31:06 -0500

    Mejora main

--- components/NavbarLandPage.jsx
+++ components/NavbarLandPage.jsx
@@ -24,24 +24,6 @@ const NavbarLandPage = () => {
           </div>
       </nav>
     </header>
-
-    // <div className={styles.containerall}>
-    //     <header className={styles.header}>
-    //       <nav className={styles.nav}>
-    //           <div>
-    //             <Image src="/img/logo_negro2.png" alt="Logosímbolo SENA Negro" className="nav__img" width={120} height={60}/>
-    //           </div>
-    //           <div className="nav__links">
-    //               <Link href="/login" className="nav__link">
-    //                   Iniciar sesión
-    //               </Link>
-    //               <Link href="/" className="nav__link nav__link--activate">
-    //                   Registrarse
-    //               </Link>
-    //           </div>
-    //       </nav>
-    //     </header>
-    // </div>
   );
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -30,37 +30,40 @@ const LandPage = () => {
 
         {/* =================== Sección Tarjetas de información ================ */}
         <section className={styles.sectioncards}>
+          {/* Tarjeta 1 */}
           <div className={styles.containerone}>
             <div className={styles.cardone}>
               <h2 className={styles.card__title}>Aprendiz</h2>
               <h3 className={styles.card__subtitle}>Comunicación efectiva</h3>
               <p className={styles.card__description}>Comunica tus inconvenientes fácilmente. Anexa excusas, el instructor  las valida, garantizando que puedas continuar con confianza.</p>
             </div>
-            <div className={styles.cardone}>
+
+            {/* Tarjeta 2 */}
+            <div className={`${styles.cardone} ${styles['cardone--blue']}`}>
               <h2 className={styles.card__title}>Aprendiz</h2>
-              <h3 className={styles.card__subtitle}>Comunicación efectiva</h3>
-              <p className={styles.card__description}>Comunica tus inconvenientes fácilmente. Anexa excusas, el instructor  las valida, garantizando que puedas continuar con confianza.</p>
+              <h3 className={styles.card__subtitle}>Control de asistencia</h3>
+              <p className={styles.card__description}>Verifica tu asistencia al instante desde la plataforma y descárguela en PDF para un seguimiento sin complicaciones.</p>
             </div>
           </div>
 
           <div className={styles.containerone}>
-            <div className={styles.cardone}>
-                <h2 className={styles.card__title}>Aprendiz</h2>
-                <h3 className={styles.card__subtitle}>Comunicación efectiva</h3>
-                <p className={styles.card__description}>Comunica tus inconvenientes fácilmente. Anexa excusas, el instructor  las valida, garantizando que puedas continuar con confianza.</p>
+            <div className={`${styles.cardone} ${styles['cardone--blue']}`}>
+                <h2 className={styles.card__title}>Instructor</h2>
+                <h3 className={styles.card__subtitle}>Control Estudiantes</h3>
+                <p className={styles.card__description}>Mantén un seguimiento organizado de la asistencia diaria de tus estudiantes con facilidad.</p>
             </div>
-            <div className={styles.cardone}>
-              <h2 className={styles.card__title}>Aprendiz</h2>
-              <h3 className={styles.card__subtitle}>Comunicación efectiva</h3>
-              <p className={styles.card__description}>Comunica tus inconvenientes fácilmente. Anexa excusas, el instructor  las valida, garantizando que puedas continuar con confianza.</p>
+            <div className={`${styles.cardone} ${styles['cardone--green']}`}>
+              <h2 className={styles.card__title}>Instructor</h2>
+              <h3 className={styles.card__subtitle}>Gestión Fichas</h3>
+              <p className={styles.card__description}>Accede y genera reportes sobre las fichas a tu cargo para un análisis detallado de tu gestión.</p>
             </div>
           </div>
 
           <div className={styles.containerthree}>
             <div className={styles.cardone}>
-              <h2 className={styles.card__title}>Aprendiz</h2>
-              <h3 className={styles.card__subtitle}>Comunicación efectiva</h3>
-              <p className={styles.card__description}>Comunica tus inconvenientes fácilmente. Anexa excusas, el instructor  las valida, garantizando que puedas continuar con confianza.</p>
+              <h2 className={styles.card__title}>Instructor</h2>
+              <h3 className={styles.card__subtitle}>Asistencia Instructor</h3>
+              <p className={styles.card__description}>Accede a tu registro de asistencia y descárgalo para un seguimiento efectivo de tus clases.</p>
             </div>
           </div>
 
