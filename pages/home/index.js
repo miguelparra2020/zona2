790bccd374c8d2915246f0b3b2dd4e7628b820c9
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-24 21:37:29 -0500

    Update

--- pages/home/index.js
+++ pages/home/index.js
@@ -11,8 +11,6 @@ const HomePage = () => {
 //---------------Variables------------------------------------------------- 
     const [ingresos, setIngresos] = useState([]);
     const [salidas, setSalidas] = useState([]);
-    //Inicializar la varialle con la fecha de hoy
-    // const [fechaFiltro, setFechaFiltro] = useState(getFormattedDate()); 
     const [fechaInicioFiltro, setFechaInicioFiltro] = useState(getFormattedDate());
     const [fechaFinFiltro, setFechaFinFiltro] = useState(getFormattedDate());
 //---------------Variables------------------------------------------------- 
@@ -64,10 +62,13 @@ const HomePage = () => {
 
     return (
         <MainLayout>
+            {/* titulo */}
             <div className="contenedor_titulo_asistencias">
                 <h1>Bienvenid@ al área de asistencias</h1>   
             </div>
+            {/* titulo */}
 
+            {/* Filtros */}
             <div className="contenedor_filtros_asistencias">
                 <div className="div_titulo_filtrar">
                     <h1>Filtrar</h1> 
@@ -94,66 +95,87 @@ const HomePage = () => {
                     
                 </div>
             </div>
+            {/* Filtros */}
 
-            <div>
-                <h1>Registros de ingresos</h1>   
-                
-                {ingresos
-                    .filter((ingreso) => {
-                        if (!fechaInicioFiltro || !fechaFinFiltro) return true; // Mostrar todo si no se ingresaron fechas
-                        return (
-                            ingreso.fecha_ingreso >= fechaInicioFiltro &&
-                            ingreso.fecha_ingreso <= fechaFinFiltro
-                        );
-                    })
-                    .map((ingreso) => {
-                // Extraer el ID de la URL
-                const urlParts = ingreso.url.split('/');
-                const id = urlParts[urlParts.length - 2]; // Suponemos que el ID está antes del último slash
+            {/* registros */}
+            <div className="contenedor_registros">
 
-                return (
-                    <div key={ingreso.url}>
-                        <p>icono: src:asdsadasd</p>
-                        <p>Id ingreso: {id}</p>
-                        <p>Usuario: {ingreso.username}</p>
-                        <p>Nombre: Miguel Ángel Páez parra</p>
-                        <p>Fecha de ingreso: {ingreso.fecha_ingreso}</p>
-                        <p>Hora hora de ingreso: {ingreso.hora_ingreso}</p>
-                        <p>zona: {ingreso.zona}</p>
-                        <Link href={`/ingresos/${id}`}>Editar</Link>
+                {/* registros de ingresos*/}
+                <div className="contenedor_registro_ingresos">
+                    <div className="div_titulo_registros">
+                        <h1>Registros de ingresos</h1> 
                     </div>
-                );
-            })}
-            </div>
-            <div>
-                <h1>Registros de Salidas</h1>  
-                {salidas
-                    .filter((salida) => {
-                        if (!fechaInicioFiltro || !fechaFinFiltro) return true; // Mostrar todo si no se ingresaron fechas
-                        return (
-                            salida.fecha_salida >= fechaInicioFiltro &&
-                            salida.fecha_salida <= fechaFinFiltro
-                        );
-                    })
-                    .map((salida) => {
+                    <div className="div_contenedor_card_registros">
+                    {ingresos
+                        .filter((ingreso) => {
+                            if (!fechaInicioFiltro || !fechaFinFiltro) return true; // Mostrar todo si no se ingresaron fechas
+                            return (
+                                ingreso.fecha_ingreso >= fechaInicioFiltro &&
+                                ingreso.fecha_ingreso <= fechaFinFiltro
+                            );
+                        })
+                        .map((ingreso) => {
                     // Extraer el ID de la URL
-                    const urlParts = salida.url.split('/');
+                    const urlParts = ingreso.url.split('/');
                     const id = urlParts[urlParts.length - 2]; // Suponemos que el ID está antes del último slash
 
                     return (
-                        <div key={salida.url}>
-                            <p>icono: src:asdsadasd</p>
-                            <p>Id salida: {id}</p>
-                            <p>Usuario: {salida.username}</p>
-                            <p>Nombre: Miguel Ángel Páez parra</p>
-                            <p>fecha_salida: {salida.fecha_salida}</p>
-                            <p>hora_salida: {salida.hora_salida}</p>
-                            <p>zona: {salida.zona}</p>
-                            <Link href={`/salidas/${id}`}>Editar</Link>
-                        </div>
-                    );
-                })}
+                            <div key={ingreso.url}>
+                                <div className="div_card">
+                                    <p>icono:</p>
+                                    <p>Id ingreso: {id}</p>
+                                    <p>Usuario: {ingreso.username}</p>
+                                    <p>Nombre: Miguel Ángel Páez parra</p>
+                                    <p>Fecha de ingreso: {ingreso.fecha_ingreso}</p>
+                                    <p>Hora hora de ingreso: {ingreso.hora_ingreso}</p>
+                                    <p>zona: {ingreso.zona}</p>
+                                    <Link href={`/ingresos/${id}`}>Editar</Link>
+                                </div>
+                            </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                {/* registros de ingresos*/}
+
+                {/* registros de salidas*/}
+                <div className="contenedor_registro_salidas">
+                    <div className="div_titulo_registros">
+                        <h1>Registros de Salidas</h1>  
+                    </div>
+                    <div className="div_contenedor_card_registros">
+                    {salidas
+                        .filter((salida) => {
+                            if (!fechaInicioFiltro || !fechaFinFiltro) return true; // Mostrar todo si no se ingresaron fechas
+                            return (
+                                salida.fecha_salida >= fechaInicioFiltro &&
+                                salida.fecha_salida <= fechaFinFiltro
+                            );
+                        })
+                        .map((salida) => {
+                        // Extraer el ID de la URL
+                        const urlParts = salida.url.split('/');
+                        const id = urlParts[urlParts.length - 2]; // Suponemos que el ID está antes del último slash
+
+                        return (
+                                <div key={salida.url}>
+                                    <p>icono: src:asdsadasd</p>
+                                    <p>Id salida: {id}</p>
+                                    <p>Usuario: {salida.username}</p>
+                                    <p>Nombre: Miguel Ángel Páez parra</p>
+                                    <p>fecha_salida: {salida.fecha_salida}</p>
+                                    <p>hora_salida: {salida.hora_salida}</p>
+                                    <p>zona: {salida.zona}</p>
+                                    <Link href={`/salidas/${id}`}>Editar</Link>
+                                </div>
+                                );
+                            })}
+                    </div>                    
+                </div>
+                {/* registros de salidas*/}
+
             </div>
+            {/* registros */}
         </MainLayout>
     )
 };
