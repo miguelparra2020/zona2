18cc70c631ca4874155740df0cc13a39d0f67590
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-26 16:46:02 -0500

    Update

--- pages/download-asistencias-pdf/index.js
+++ pages/download-asistencias-pdf/index.js
@@ -1,27 +1,17 @@
 import React from "react";
 import MainLayout from '../../components/layouts/MainLayout';
-import '../../styles/pages/fichas.css';
-import { PDFViewer } from '@react-pdf/renderer';
-import { PDFDownloadLink } from '@react-pdf/renderer';
+// import '../../styles/pages/fichas.css';
+// import { PDFViewer } from '@react-pdf/renderer';
+// import { PDFDownloadLink } from '@react-pdf/renderer';
 
-import MyDocumentFichas from './documento-fichas';
+// import MyDocumentFichas from './documento-fichas';
 
 function DowloadAsistenciasPage(){
 
 //---área visual de la página---------
     return (
         <MainLayout>
-            <PDFDownloadLink
-                document={<MyDocumentFichas />}
-                fileName="fichas.pdf"
-            >
-                {({ blob, url, loading, error }) =>
-                    loading ? 'Generando PDF...' : 'Descargar PDF'
-                }
-            </PDFDownloadLink>
-            <PDFViewer>
-                <MyDocumentFichas />
-            </PDFViewer>
+            Hola soy DowloadAsistenciasPage
             
         </MainLayout>
     )
