e70eabf2891ab9f7d4d8caf0891a62eb134ac5fa
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-26 16:55:03 -0500

    Update

--- pages/download-fichas-pdf/index.js
+++ pages/download-fichas-pdf/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import MainLayout from '../../components/layouts/MainLayout';
 import '../../styles/pages/fichas.css';
 import { PDFViewer } from '@react-pdf/renderer';
-import { PDFDownloadLink } from '@react-pdf/renderer';
+// import { PDFDownloadLink } from '@react-pdf/renderer';
 
 import MyDocumentFichas from './documento-fichas';
 
@@ -11,14 +11,7 @@ function DowloadFichasPage(){
 //---área visual de la página---------
     return (
         <MainLayout>
-            <PDFDownloadLink
-                document={<MyDocumentFichas />}
-                fileName="fichas.pdf"
-            >
-                {({ blob, url, loading, error }) =>
-                    loading ? 'Generando PDF...' : 'Descargar PDF'
-                }
-            </PDFDownloadLink>
+            
             <PDFViewer>
                 <MyDocumentFichas />
             </PDFViewer>
