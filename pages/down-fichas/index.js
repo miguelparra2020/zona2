e0bf7dbd1b0d69dcd440bc16e5bedf5d494a3880
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-26 22:02:34 -0500

    Update

--- pages/down-fichas/index.js
+++ pages/down-fichas/index.js
@@ -2,16 +2,22 @@ import React from "react";
 import MainLayout from '../../components/layouts/MainLayout';
 import MyDocument from './down-component';
 import { PDFDownloadLink } from "@react-pdf/renderer";
-
+import { Suspense } from 'react';
 
 function DownFichasPage () {
     return (
             <MainLayout>
-                <PDFDownloadLink document={<MyDocument/>} >
+                <Suspense fallback={<Loading />}>
+                    <PDFDownloadLink document={<MyDocument/>} >
                     descargar
-                    </PDFDownloadLink>
+                    </PDFDownloadLink></Suspense>
+                
             </MainLayout>
     )
 };
 
 export default DownFichasPage;
+
+function Loading() {
+    return <h2>🌀 Loading...</h2>;
+  }
