11940a5da73aa0edb863d1553aa7041eab0fd4dc
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-26 21:52:06 -0500

    Update

--- pages/down-fichas/index.js
+++ pages/down-fichas/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import MainLayout from '../../components/layouts/MainLayout';
 import MyDocument from './down-component';
-import { PDFViewer } from '@react-pdf/renderer';
 
 
 
 function DownFichasPage () {
     return (
             <MainLayout>
-                <PDFViewer>
                     <MyDocument/>
-                </PDFViewer>
             </MainLayout>
     )
 };
