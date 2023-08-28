5baa6a478a37de8910f374f9e28f354bfcbe22ac
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-08-28 10:16:41 -0500

    Update

--- pages/miperfil/index.js
+++ pages/miperfil/index.js
@@ -1,45 +1,14 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/router';
+import React from "react";
+import MainLayout from '../../components/layouts/MainLayout';
 
-const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
-  const handleLogin = async () => {
-    try {
-      const response = await axios.post('https://miguelpaez9612.pythonanywhere.com/api/token/', {
-        username: username,
-        password: password,
-      });
-
-      const accessToken = response.data.access;
-      console.log('Token de acceso:', accessToken);
-      router.push('/home');
-
-      // Aquí puedes guardar el token en el almacenamiento local o en una cookie
-    } catch (error) {
-      console.error('Error al iniciar sesión:', error);
-    }
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Usuario"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Contraseña"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Iniciar Sesión</button>
-    </div>
-  );
+const MiPerfilPage = () => {
+    return (
+        <MainLayout>
+            <div>
+                <h1>Hola soy MiPerfilPage</h1>   
+            </div>
+        </MainLayout>
+    )
 };
 
-export default LoginPage;
+export default MiPerfilPage;
