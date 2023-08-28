1fd0b8841621eb9ca5fd7f8468ed0e393d212125
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-28 17:43:47 -0500

    update

---
+++ pages/usuarios/[id].js
@@ -0,0 +1,15 @@
+import React from "react";
+import MainLayout from '../../components/layouts/MainLayout';
+
+const EditarUsuarioPage = () => {
+    return (
+        <MainLayout>
+            <div>
+                <h1>Hola soy Editar usuario</h1>
+                
+            </div>
+        </MainLayout>
+    )
+};
+
+export default EditarUsuarioPage;
