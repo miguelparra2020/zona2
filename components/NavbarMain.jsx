069d49a77f07cac409f351236b7a6ae66d9c51b3
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-29 22:55:23 -0500

    Update

--- components/NavbarMain.jsx
+++ components/NavbarMain.jsx
@@ -6,54 +6,82 @@ import styles from '@/styles/components/NavbarMain.module.css';
 import '../styles/global.css';
 
 
+
 const NavbarMain = () => {
   return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <div>
-          <Link href="/home" className={styles.nav__link}>   
-            <h1>
-              Bs
-            </h1>
-          </Link>
-        </div>
-        <div>
-          <Link href="/home" className={styles.nav__link}> 
-              asistencias
-          </Link>
-        </div>
-        <div >
-          <Link href="/excusas" className={styles.nav__link}>     
-              <div>Excusas</div>
-          </Link>
-        </div>
-        <div>
-          <Link href="/miqr" className={styles.nav__link}>     
-              <div>Mi Qr</div>
-          </Link>
-        </div>
-        <div>
-          <Link href="/miperfil" className={styles.nav__link}>     
-              <div>Mi Perfil</div>
-          </Link>
-        </div>
-        <div>
-          <Link href="/fichas" className={styles.nav__link}>     
-              <div>Fichas</div>
-          </Link>
-        </div>
-        <div>
-          <Link href="/usuarios" className={styles.nav__link}>     
-              <div>Usuarios</div>
-          </Link>
-        </div>
-        <div>
-          <Link href="/" className={styles.nav__link}>     
-              <div>Salir</div>
-          </Link>
-        </div>
-      </nav>
-    </header>
+    <div className={styles.header}>
+      <div className={styles.nav}>
+        <Link href="/home" className={styles.links}> 
+          <div className={styles.nav__link}>
+                Bs            
+          </div>
+        </Link>
+        <Link href="/home" className={styles.links}> 
+          <div className={styles.nav__link}>            
+                asistencias &nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+  <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+  <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
+                </svg>           
+          </div>
+        </Link>
+        <Link href="/excusas" className={styles.links}>
+          <div className={styles.nav__link}>                
+                Excusas  &nbsp;   
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf" viewBox="0 0 16 16">
+  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+  <path d="M4.603 14.087a.81.81 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.68 7.68 0 0 1 1.482-.645 19.697 19.697 0 0 0 1.062-2.227 7.269 7.269 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.188-.012.396-.047.614-.084.51-.27 1.134-.52 1.794a10.954 10.954 0 0 0 .98 1.686 5.753 5.753 0 0 1 1.334.05c.364.066.734.195.96.465.12.144.193.32.2.518.007.192-.047.382-.138.563a1.04 1.04 0 0 1-.354.416.856.856 0 0 1-.51.138c-.331-.014-.654-.196-.933-.417a5.712 5.712 0 0 1-.911-.95 11.651 11.651 0 0 0-1.997.406 11.307 11.307 0 0 1-1.02 1.51c-.292.35-.609.656-.927.787a.793.793 0 0 1-.58.029zm1.379-1.901c-.166.076-.32.156-.459.238-.328.194-.541.383-.647.547-.094.145-.096.25-.04.361.01.022.02.036.026.044a.266.266 0 0 0 .035-.012c.137-.056.355-.235.635-.572a8.18 8.18 0 0 0 .45-.606zm1.64-1.33a12.71 12.71 0 0 1 1.01-.193 11.744 11.744 0 0 1-.51-.858 20.801 20.801 0 0 1-.5 1.05zm2.446.45c.15.163.296.3.435.41.24.19.407.253.498.256a.107.107 0 0 0 .07-.015.307.307 0 0 0 .094-.125.436.436 0 0 0 .059-.2.095.095 0 0 0-.026-.063c-.052-.062-.2-.152-.518-.209a3.876 3.876 0 0 0-.612-.053zM8.078 7.8a6.7 6.7 0 0 0 .2-.828c.031-.188.043-.343.038-.465a.613.613 0 0 0-.032-.198.517.517 0 0 0-.145.04c-.087.035-.158.106-.196.283-.04.192-.03.469.046.822.024.111.054.227.09.346z"/>
+                </svg>       
+          </div>
+        </Link>
+        <Link href="/miqr" className={styles.links}> 
+          <div className={styles.nav__link}>                
+                Mi Qr &nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-qr-code-scan" viewBox="0 0 16 16">
+  <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0v-3Zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5ZM.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5Zm15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5ZM4 4h1v1H4V4Z"/>
+  <path d="M7 2H2v5h5V2ZM3 3h3v3H3V3Zm2 8H4v1h1v-1Z"/>
+  <path d="M7 9H2v5h5V9Zm-4 1h3v3H3v-3Zm8-6h1v1h-1V4Z"/>
+  <path d="M9 2h5v5H9V2Zm1 1v3h3V3h-3ZM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8H8Zm2 2H9V9h1v1Zm4 2h-1v1h-2v1h3v-2Zm-4 2v-1H8v1h2Z"/>
+  <path d="M12 9h2V8h-2v1Z"/>
+                </svg>            
+          </div>
+        </Link>
+        <Link href="/miperfil" className={styles.links}> 
+          <div className={styles.nav__link}>                
+                Mi Perfil &nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
+  <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
+  <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>           
+          </div>
+        </Link>
+        <Link href="/fichas" className={styles.links}>  
+          <div className={styles.nav__link}>
+                Fichas &nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-layers-half" viewBox="0 0 16 16">
+  <path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/>
+                </svg>       
+          </div>
+        </Link>
+        <Link href="/usuarios" className={styles.links}>  
+          <div className={styles.nav__link}>
+            Usuarios &nbsp;
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
+            </svg>
+          </div>               
+        </Link>
+        <Link href="/" className={styles.links}>   
+          <div className={styles.nav__link}>            
+              Salir &nbsp;
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+  <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+              </svg>         
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- pages/excusas/index.js
+++ pages/excusas/index.js
@@ -4,9 +4,39 @@ import MainLayout from '../../components/layouts/MainLayout';
 const ExcusasPage = () => {
     return (
         <MainLayout>
+            {/* titulo del área */}
             <div>
-                <h1>Hola soy Excusas</h1>   
+                <h1>Bienvenido al área de excusas</h1>   
             </div>
+            {/* titulo del área */}
+            
+            {/* card de la excusa */}
+            <div>
+                <div>
+                    icon 
+                    id: id excusa
+                </div>
+                <div>
+                    usuario: 1088336464
+                    nombre: Miguel Páez
+                </div>
+                <div>
+                    excusa doc
+                </div>
+                <div>
+                    comentario del usuario:
+                </div>
+                <div>
+                    Revisó, aprobó o desaprobó:
+                    usuario:
+                    nombre:
+                </div>
+                <div>
+                    Estado:
+                    Comentario:
+                </div>
+            </div>
+            {/* card de la excusa */}
         </MainLayout>
     )
 };
