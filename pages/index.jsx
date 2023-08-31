f0e33f6e4cb88d5316d8d013d1a47db53396b525
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-31 15:21:35 -0500

    Update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,52 @@
-import React, { useState } from 'react';
 import  LoginLayout from '../components/layouts/LoginLayout';
+import React, { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+
+const DynamicQrReader = dynamic(() => import('react-qr-scanner'), { ssr: false });
+
 
 const IndexPage = () => {
+  const [scanning, setScanning] = useState(true);
+  const [result, setResult] = useState(null);
+
+  const handleScan = (data) => {
+    if (data) {
+      setResult(data.text); // Usamos solo la propiedad 'text' del objeto QR
+      setScanning(false);
+    }
+  };
+
+  const handleError = (error) => {
+    console.error(error);
+    setScanning(false);
+  };
+
+  useEffect(() => {
+    setScanning(true);
+  }, []);
+
 
   return (
     <LoginLayout>
       Hola soy Index Zona 1
+      <div>
+      {scanning && (
+        <DynamicQrReader
+          delay={100}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ width: '50%' }}
+        />
+      )}
+      {result && <p>Resultado: {result}</p>}
+    </div>
     </LoginLayout>
   );
 };
 
 export default IndexPage;
+
+
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
+}
